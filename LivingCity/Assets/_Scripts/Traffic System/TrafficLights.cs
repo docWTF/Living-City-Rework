cc9993a5c324c82e5cf131f363de80b3f59d3164
docWTF <henrydo1999@gmail.com>
2024-01-20 03:36:03 +0700

    Updated Cars Physics

--- LivingCity/Assets/_Scripts/Traffic System/TrafficLights.cs
+++ LivingCity/Assets/_Scripts/Traffic System/TrafficLights.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +16,11 @@ public class TrafficLights : MonoBehaviour
     private int timerYellow = 5;
     [SerializeField]
     private int timerRed = 25;
+    [SerializeField]
+    private int timerWalk = 6;
+    [SerializeField]
+    private GameObject redLight, yellowLight, greenLight;
+
     public enum TrafficLightColor
     {
         Green,
@@ -33,32 +39,35 @@ public class TrafficLights : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(TrafficLightsTimer(this, timerGreen, timerYellow, timerRed, startColor));
+        StartCoroutine(TrafficLightsTimer(this, timerGreen, timerYellow, timerRed, timerWalk, startColor));
     }
 
-    IEnumerator TrafficLightsTimer(TrafficLights trafficLightObject,int timerGreen, int timerYellow, int timerRed, TrafficLightStart startColor)
+    IEnumerator TrafficLightsTimer(TrafficLights trafficLightObject,int timerGreen, int timerYellow, int timerRed, int timerWalk, TrafficLightStart startColor)
     {
             if (startColor == TrafficLightStart.StartGreen)
             {
                 while (true)
                 {
-                Debug.Log("Turn Green");
+                    Debug.Log("Turn Green");
                     trafficLightObject.GetComponent<BoxCollider>().enabled = false;
                     trafficLightObject.lightColor = TrafficLightColor.Green;
-
+                    TrafficLightChangeColor(lightColor);
                     yield return new WaitForSeconds(timerGreen);
 
-                Debug.Log("Turn Yellow");
+                    Debug.Log("Turn Yellow");
                     trafficLightObject.GetComponent<BoxCollider>().enabled = true;
                     trafficLightObject.lightColor = TrafficLightColor.Yellow;
-
+                    TrafficLightChangeColor(lightColor);
                     yield return new WaitForSeconds(timerYellow);
 
 
-                Debug.Log("Turn Red");
-                trafficLightObject.lightColor = TrafficLightColor.Red;
-
+                    Debug.Log("Turn Red");
+                    trafficLightObject.lightColor = TrafficLightColor.Red;
+                    TrafficLightChangeColor(lightColor);
                     yield return new WaitForSeconds(timerRed);
+                    yield return new WaitForSeconds(timerWalk);
+
+
                 }
 
             }
@@ -66,29 +75,49 @@ public class TrafficLights : MonoBehaviour
             {
                 while(true)
                 {
-                Debug.Log("Turn Yellow");
+                Debug.Log("Turn Red");
                 trafficLightObject.GetComponent<BoxCollider>().enabled = true;
                 trafficLightObject.lightColor = TrafficLightColor.Red;
-
+                TrafficLightChangeColor(lightColor);
                 yield return new WaitForSeconds(timerRed);
+                yield return new WaitForSeconds(timerWalk);
 
                 Debug.Log("Turn Green");
                 trafficLightObject.GetComponent<BoxCollider>().enabled = false;
                 trafficLightObject.lightColor = TrafficLightColor.Green;
-
+                TrafficLightChangeColor(lightColor);
                 yield return new WaitForSeconds(timerGreen);
 
-                Debug.Log("Turn Red");
+                Debug.Log("Turn Yellow");
                 trafficLightObject.GetComponent<BoxCollider>().enabled = true;
                 trafficLightObject.lightColor = TrafficLightColor.Yellow;
-
+                TrafficLightChangeColor(lightColor);
                 yield return new WaitForSeconds(timerYellow);
 
                 }
             }
     }
 
+    private void TrafficLightChangeColor(TrafficLightColor lightColor)
+    {
+        if (lightColor == TrafficLightColor.Red)
+        {
+            redLight.SetActive(true);
+            yellowLight.SetActive(false);
+            greenLight.SetActive(false);
+        }
+        else if (lightColor == TrafficLightColor.Yellow)
+        {
+            redLight.SetActive(false);
+            yellowLight.SetActive(true);
+            greenLight.SetActive(false);
+        }
+        else if (lightColor == TrafficLightColor.Green)
+        {
+            redLight.SetActive(false);
+            yellowLight.SetActive(false);
+            greenLight.SetActive(true);
 
-
-
+        }
+    }
 }
